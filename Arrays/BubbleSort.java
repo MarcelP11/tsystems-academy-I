5bac335e914130af59ea1a2c2de43d7141436dbe
Marcel Polivcak <marcel.polivcak@centrum.sk>
2022-07-10 22:21:49 +0200

    matrice a bublesort

--- Arrays/BubbleSort.java
+++ Arrays/BubbleSort.java
@@ -1,6 +1,30 @@
 public class BubbleSort {
-    public static void
+    public static void bubble(int x[]) {   //parameter je pole
+        int counter = x.length;  //do novej premennej ulozim hodnotu dlzky pola
+
+        for (int k = 0; k < counter; counter--) {   //slucka for pre posuvanie limitu
+            for (int i = 0, j = 1; j < counter; i++, j++) {  //slucka for pre porovnavanie susednych hodnot
+                int a = x[i];   //do premennych ukladam hodnoty zo susednych poli
+                int b = x[j];
+                if (x[i] > x[j]) {    //ak je lava strana vacsia ako prava tak sa hodnoty v poliach vymenia
+                    x[i] = b;
+                    x[j] = a;
+                }
+            }
+        }
+    }
+
+    public static void printValues(int x[]) {
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i] + ",");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        int src[]={5,7,1,3,4,8,9,0,3,7};
+        int src[] = {5, 7, 1, 3, 4, 8, 9, 0, 3, 7};
+        printValues(src);
+        bubble(src);
+        printValues(src);
     }
 }
--- Arrays/Matrices.java
+++ Arrays/Matrices.java
@@ -1,2 +1,93 @@
-package PACKAGE_NAME;public class Matrices {
+public class Matrices {
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    //scitanie matric
+    public static int[][] addMatrices(int[][] matrixA, int[][] matrixB) {
+        int[][] matrixC = new int[4][5];
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[i].length; j++) {
+                matrixC[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+        return matrixC;
+    }
+
+    //odcitanie matric
+    public static int[][] subtractMatrices(int[][] matrixA, int[][] matrixB) {
+        int[][] matrixC = new int[4][5];
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[i].length; j++) {
+                matrixC[i][j] = matrixA[i][j] - matrixB[i][j];
+            }
+        }
+        return matrixC;
+    }
+
+    //transpozicia matric
+    public static int[][] transpose(int[][] matrixA) {
+        int[][] matrixC = new int[5][4];
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[i].length; j++) {
+                matrixC[j][i] = matrixA[i][j];
+            }
+        }
+        return matrixC;
+    }
+
+    //sucin matric
+    public static int[][] multiply(int[][] matrixA, int[][] matrixB) {
+        int[][] matrixC = new int[4][5];
+
+        for (int k = 0; k < matrixC.length; k++) {
+            for (int j = 0; j < matrixC[k].length; j++) {
+                int a = 0;
+                for (int i = 0; i < matrixA[k].length; i++) {
+                    for (int h = 0; h < matrixB[j].length; h++) {
+                        int midResult = matrixA[k][i] * matrixB[h][j];
+                        a = +midResult;
+                    }
+                }
+                matrixC[k][j] = a;
+            }
+        }
+        return matrixC;
+    }
+
+    public static void main(String[] args) {
+        int[][] aMatrix = new int[4][5];
+        int[][] bMatrix = new int[4][5]; //vytvorim si 2 matice 4x5
+
+        //naplnim hodnotami obe polia a v matrici b zacnem od 1
+        for (int i = 0; i < aMatrix.length; i++) {
+            for (int j = 0; j < aMatrix[i].length; j++) {
+                aMatrix[i][j] = i + j;
+            }
+        }
+        for (int i = 0; i < bMatrix.length; i++) {
+            for (int j = 0; j < bMatrix[i].length; j++) {
+                bMatrix[i][j] = i + j + 1;
+            }
+        }
+        printMatrix(aMatrix);
+        printMatrix(bMatrix);
+
+        int[][] addition = addMatrices(aMatrix, bMatrix);  //vytvorenie suctu
+        printMatrix(addition);  //vypisanie suctu
+        int[][] subtraction = subtractMatrices(aMatrix, bMatrix);  //vytvorenie odciatania
+        printMatrix(subtraction);  //vypisanie odcitania
+        int[][] transposeA = transpose(aMatrix);  //transpozicia
+        int[][] transposeB = transpose(aMatrix);
+        printMatrix(transposeA);
+        printMatrix(transposeB);
+        int[][] multiplication = multiply(aMatrix, bMatrix);
+        printMatrix(multiplication);
+    }
 }
