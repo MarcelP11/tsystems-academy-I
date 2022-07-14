26309018a14d53ba302e90a13cc48b158f634251
Marcel Polivcak <marcel.polivcak@centrum.sk>
2022-07-14 15:28:19 +0200

    14072022

---
+++ UsefulJavaClasses/countries/Countries.java
@@ -0,0 +1,263 @@
+package countries;
+
+public class Countries {
+    public static void main(String[] args) {
+
+        // regex \d{3}\t[A-Z]{3}\s([A-Z]{2})\t\(ISO 3166-2\)\t(.{0,})
+       // regex [^\t]{3}\t([^\t]{3})\t([^\t]{2})\t[^\t]{12}\t(.{0,})
+        Country[] country = new Country();
+        country[]={
+        004	AFG	AF	(ISO 3166-2)	Afganistan
+        248	ALA	AX	(ISO 3166-2)	Alandy
+        008	ALB	AL	(ISO 3166-2)	Albánsko
+        012	DZA	DZ	(ISO 3166-2)	Alžírsko
+        016	ASM	AS	(ISO 3166-2)	Americká Samoa
+        850	VIR	VI	(ISO 3166-2)	Americké Panenské ostrovy
+        020	AND	AD	(ISO 3166-2)	Andorra
+        024	AGO	AO	(ISO 3166-2)	Angola
+        660	AIA	AI	(ISO 3166-2)	Anguilla
+        010	ATA	AQ	(ISO 3166-2)	Antarktída
+        028	ATG	AG	(ISO 3166-2)	Antigua a Barbuda
+        032	ARG	AR	(ISO 3166-2)	Argentína
+        051	ARM	AM	(ISO 3166-2)	Arménsko
+        533	ABW	AW	(ISO 3166-2)	Aruba
+        036	AUS	AU	(ISO 3166-2)	Austrália
+        031	AZE	AZ	(ISO 3166-2)	Azerbajdžan
+        044	BHS	BS	(ISO 3166-2)	Bahamy
+        048	BHR	BH	(ISO 3166-2)	Bahrajn
+        050	BGD	BD	(ISO 3166-2)	Bangladéš
+        052	BRB	BB	(ISO 3166-2)	Barbados
+        056	BEL	BE	(ISO 3166-2)	Belgicko
+        084	BLZ	BZ	(ISO 3166-2)	Belize
+        204	BEN	BJ	(ISO 3166-2)	Benin
+        060	BMU	BM	(ISO 3166-2)	Bermudy
+        064	BTN	BT	(ISO 3166-2)	Bhután
+        112	BLR	BY	(ISO 3166-2)	Bielorusko
+        068	BOL	BO	(ISO 3166-2)	Bolívia
+        535	BES	BQ	(ISO 3166-2)	Bonaire, Svätý Eustach a Saba
+        070	BIH	BA	(ISO 3166-2)	Bosna a Hercegovina
+        072	BWA	BW	(ISO 3166-2)	Botswana
+        074	BVT	BV	(ISO 3166-2)	Bouvetov ostrov
+        076	BRA	BR	(ISO 3166-2)	Brazília
+        086	IOT	IO	(ISO 3166-2)	Britské indickooceánske územie
+        092	VGB	VG	(ISO 3166-2)	Britské Panenské ostrovy
+        096	BRN	BN	(ISO 3166-2)	Brunej
+        100	BGR	BG	(ISO 3166-2)	Bulharsko
+        854	BFA	BF	(ISO 3166-2)	Burkina
+        108	BDI	BI	(ISO 3166-2)	Burundi
+        184	COK	CK	(ISO 3166-2)	Cookove ostrovy
+        531	CUW	CW	(ISO 3166-2)	Curaçao
+        196	CYP	CY	(ISO 3166-2)	Cyprus
+        148	TCD	TD	(ISO 3166-2)	Čad
+        203	CZE	CZ	(ISO 3166-2)	Česko
+        499	MNE	ME	(ISO 3166-2)	Čierna Hora
+        152	CHL	CL	(ISO 3166-2)	Čile
+        156	CHN	CN	(ISO 3166-2)	Čína
+        208	DNK	DK	(ISO 3166-2)	Dánsko
+        212	DMA	DM	(ISO 3166-2)	Dominika
+        214	DOM	DO	(ISO 3166-2)	Dominikánska republika
+        262	DJI	DJ	(ISO 3166-2)	Džibutsko
+        818	EGY	EG	(ISO 3166-2)	Egypt
+        218	ECU	EC	(ISO 3166-2)	Ekvádor
+        232	ERI	ER	(ISO 3166-2)	Eritrea
+        233	EST	EE	(ISO 3166-2)	Estónsko
+        231	ETH	ET	(ISO 3166-2)	Etiópia
+                ???	???	EU	(ISO 3166-2)	Európska únia
+        234	FRO	FO	(ISO 3166-2)	Faerské ostrovy
+        238	FLK	FK	(ISO 3166-2)	Falklandy
+        242	FJI	FJ	(ISO 3166-2)	Fidži
+        608	PHL	PH	(ISO 3166-2)	Filipíny
+        246	FIN	FI	(ISO 3166-2)	Fínsko
+        250	FRA	FR	(ISO 3166-2)	Francúzsko
+        254	GUF	GF	(ISO 3166-2)	Francúzska Guyana
+        258	PYF	PF	(ISO 3166-2)	Francúzska Polynézia
+        260	ATF	TF	(ISO 3166-2)	Francúzske južné územia
+        266	GAB	GA	(ISO 3166-2)	Gabon
+        270	GMB	GM	(ISO 3166-2)	Gambia
+        288	GHA	GH	(ISO 3166-2)	Ghana
+        292	GIB	GI	(ISO 3166-2)	Gibraltár
+        300	GRC	GR	(ISO 3166-2)	Grécko
+        308	GRD	GD	(ISO 3166-2)	Grenada
+        304	GRL	GL	(ISO 3166-2)	Grónsko
+        268	GEO	GE	(ISO 3166-2)	Gruzínsko
+        312	GLP	GP	(ISO 3166-2)	Guadeloupe
+        316	GUM	GU	(ISO 3166-2)	Guam
+        320	GTM	GT	(ISO 3166-2)	Guatemala
+        831	GGY	GG	(ISO 3166-2)	Guernsey
+        324	GIN	GN	(ISO 3166-2)	Guinea
+        624	GNB	GW	(ISO 3166-2)	Guinea-Bissau
+        328	GUY	GY	(ISO 3166-2)	Guyana
+        332	HTI	HT	(ISO 3166-2)	Haiti
+        334	HMD	HM	(ISO 3166-2)	Heardov ostrov
+        528	NLD	NL	(ISO 3166-2)	Holandsko
+        530	ANT	AN	(ISO 3166-2)	Holandské Antily
+        340	HND	HN	(ISO 3166-2)	Honduras
+        344	HKG	HK	(ISO 3166-2)	Hongkong
+        191	HRV	HR	(ISO 3166-2)	Chorvátsko
+        356	IND	IN	(ISO 3166-2)	India
+        360	IDN	ID	(ISO 3166-2)	Indonézia
+        364	IRN	IR	(ISO 3166-2)	Irán
+        368	IRQ	IQ	(ISO 3166-2)	Irak
+        372	IRL	IE	(ISO 3166-2)	Írsko
+        352	ISL	IS	(ISO 3166-2)	Island
+        376	ISR	IL	(ISO 3166-2)	Izrael
+        388	JAM	JM	(ISO 3166-2)	Jamajka
+        392	JPN	JP	(ISO 3166-2)	Japonsko
+        887	YEM	YE	(ISO 3166-2)	Jemen
+        832	JEY	JE	(ISO 3166-2)	Jersey
+        400	JOR	JO	(ISO 3166-2)	Jordánsko
+        710	ZAF	ZA	(ISO 3166-2)	Južná Afrika
+        239	SGS	GS	(ISO 3166-2)	Južná Georgia a Južné Sandwichove ostrovy
+        728	SSD	SS	(ISO 3166-2)	Južný Sudán
+        136	CYM	KY	(ISO 3166-2)	Kajmanie ostrovy
+        116	KHM	KH	(ISO 3166-2)	Kambodža
+        120	CMR	CM	(ISO 3166-2)	Kamerun
+        124	CAN	CA	(ISO 3166-2)	Kanada
+        132	CPV	CV	(ISO 3166-2)	Kapverdy
+        634	QAT	QA	(ISO 3166-2)	Katar
+        398	KAZ	KZ	(ISO 3166-2)	Kazachstan
+        404	KEN	KE	(ISO 3166-2)	Keňa
+        417	KGZ	KG	(ISO 3166-2)	Kirgizsko
+        296	KIR	KI	(ISO 3166-2)	Kiribati
+        166	CCK	CC	(ISO 3166-2)	Kokosové ostrovy
+        170	COL	CO	(ISO 3166-2)	Kolumbia
+        174	COM	KM	(ISO 3166-2)	Komory
+        178	COG	CG	(ISO 3166-2)	Kongo
+        180	COD	CD	(ISO 3166-2)	Konžská demokratická republika
+        408	PRK	KP	(ISO 3166-2)	Kórejská ľudovodemokratická republika
+        410	KOR	KR	(ISO 3166-2)	Kórejská republika
+        188	CRI	CR	(ISO 3166-2)	Kostarika
+        192	CUB	CU	(ISO 3166-2)	Kuba
+        414	KWT	KW	(ISO 3166-2)	Kuvajt
+        418	LAO	LA	(ISO 3166-2)	Laos
+        426	LSO	LS	(ISO 3166-2)	Lesotho
+        422	LBN	LB	(ISO 3166-2)	Libanon
+        430	LBR	LR	(ISO 3166-2)	Libéria
+        434	LBY	LY	(ISO 3166-2)	Líbya
+        438	LIE	LI	(ISO 3166-2)	Lichtenštajnsko
+        440	LTU	LT	(ISO 3166-2)	Litva
+        428	LVA	LV	(ISO 3166-2)	Lotyšsko
+        442	LUX	LU	(ISO 3166-2)	Luxembursko
+        446	MAC	MO	(ISO 3166-2)	Macao
+        807	MKD	MK	(ISO 3166-2)	Severné Macedónsko
+        450	MDG	MG	(ISO 3166-2)	Madagaskar
+        348	HUN	HU	(ISO 3166-2)	Maďarsko
+        458	MYS	MY	(ISO 3166-2)	Malajzia
+        454	MWI	MW	(ISO 3166-2)	Malawi
+        462	MDV	MV	(ISO 3166-2)	Maldivy
+        466	MLI	ML	(ISO 3166-2)	Mali
+        470	MLT	MT	(ISO 3166-2)	Malta
+        504	MAR	MA	(ISO 3166-2)	Maroko
+        584	MHL	MH	(ISO 3166-2)	Marshallove ostrovy
+        474	MTQ	MQ	(ISO 3166-2)	Martinik
+        480	MUS	MU	(ISO 3166-2)	Maurícius
+        478	MRT	MR	(ISO 3166-2)	Mauritánia
+        175	MYT	YT	(ISO 3166-2)	Mayotte
+        581	UMI	UM	(ISO 3166-2)	Menšie odľahlé ostrovy USA
+        484	MEX	MX	(ISO 3166-2)	Mexiko
+        583	FSM	FM	(ISO 3166-2)	Mikronézia
+        104	MMR	MM	(ISO 3166-2)	Mjanmarsko
+        498	MDA	MD	(ISO 3166-2)	Moldavsko
+        492	MCO	MC	(ISO 3166-2)	Monako
+        496	MNG	MN	(ISO 3166-2)	Mongolsko
+        500	MSR	MS	(ISO 3166-2)	Montserrat
+        508	MOZ	MZ	(ISO 3166-2)	Mozambik
+        516	NAM	NA	(ISO 3166-2)	Namíbia
+        520	NRU	NR	(ISO 3166-2)	Nauru
+        276	DEU	DE	(ISO 3166-2)	Nemecko
+        524	NPL	NP	(ISO 3166-2)	Nepál
+        562	NER	NE	(ISO 3166-2)	Niger
+        566	NGA	NG	(ISO 3166-2)	Nigéria
+        558	NIC	NI	(ISO 3166-2)	Nikaragua
+        570	NIU	NU	(ISO 3166-2)	Niue
+        574	NFK	NF	(ISO 3166-2)	Norfolk
+        578	NOR	NO	(ISO 3166-2)	Nórsko
+        540	NCL	NC	(ISO 3166-2)	Nová Kaledónia
+        554	NZL	NZ	(ISO 3166-2)	Nový Zéland
+        512	OMN	OM	(ISO 3166-2)	Omán
+        833	IMN	IM	(ISO 3166-2)	Ostrov Man
+        586	PAK	PK	(ISO 3166-2)	Pakistan
+        585	PLW	PW	(ISO 3166-2)	Palau
+        275	PSE	PS	(ISO 3166-2)	Palestína
+        591	PAN	PA	(ISO 3166-2)	Panama
+        598	PNG	PG	(ISO 3166-2)	Papua-Nová Guinea
+        600	PRY	PY	(ISO 3166-2)	Paraguaj
+        604	PER	PE	(ISO 3166-2)	Peru
+        612	PCN	PN	(ISO 3166-2)	Pitcairnove ostrovy
+        384	CIV	CI	(ISO 3166-2)	Pobrežie Slonoviny
+        616	POL	PL	(ISO 3166-2)	Poľsko
+        630	PRI	PR	(ISO 3166-2)	Portoriko
+        620	PRT	PT	(ISO 3166-2)	Portugalsko
+        040	AUT	AT	(ISO 3166-2)	Rakúsko
+        638	REU	RE	(ISO 3166-2)	Réunion
+        226	GNQ	GQ	(ISO 3166-2)	Rovníková Guinea
+        642	ROU/ROM	RO	(ISO 3166-2)	Rumunsko
+        643	RUS	RU	(ISO 3166-2)	Rusko
+        646	RWA	RW	(ISO 3166-2)	Rwanda
+        663	MAF	MF	(ISO 3166-2)	Saint-Martin
+        666	SPM	PM	(ISO 3166-2)	Saint Pierre a Miquelon
+        222	SLV	SV	(ISO 3166-2)	Salvádor
+        882	WSM	WS	(ISO 3166-2)	Samoa
+        674	SMR	SM	(ISO 3166-2)	San Maríno
+        682	SAU	SA	(ISO 3166-2)	Saudská Arábia
+        686	SEN	SN	(ISO 3166-2)	Senegal
+        580	MNP	MP	(ISO 3166-2)	Severné Mariány
+        690	SYC	SC	(ISO 3166-2)	Seychely
+        694	SLE	SL	(ISO 3166-2)	Sierra Leone
+        702	SGP	SG	(ISO 3166-2)	Singapur
+        534	SXM	SX	(ISO 3166-2)	Sint Maarten
+        703	SVK	SK	(ISO 3166-2)	Slovensko
+        705	SVN	SI	(ISO 3166-2)	Slovinsko
+        706	SOM	SO	(ISO 3166-2)	Somálsko
+        784	ARE	AE	(ISO 3166-2)	Spojené arabské emiráty
+        826	GBR	GB	(ISO 3166-2)	Spojené kráľovstvo
+        840	USA	US	(ISO 3166-2)	Spojené štáty
+        688	SRB	RS	(ISO 3166-2)	Srbsko
+        144	LKA	LK	(ISO 3166-2)	Srí Lanka
+        140	CAF	CF	(ISO 3166-2)	Stredoafrická republika
+        729	SDN	SD	(ISO 3166-2)	Sudán
+        740	SUR	SR	(ISO 3166-2)	Surinam
+        654	SHN	SH	(ISO 3166-2)	Svätá Helena
+        662	LCA	LC	(ISO 3166-2)	Svätá Lucia
+        652	BLM	BL	(ISO 3166-2)	Svätý Bartolomej
+        659	KNA	KN	(ISO 3166-2)	Svätý Krištof a Nevis
+        678	STP	ST	(ISO 3166-2)	Svätý Tomáš a Princov ostrov
+        670	VCT	VC	(ISO 3166-2)	Svätý Vincent a Grenadíny
+        748	SWZ	SZ	(ISO 3166-2)	Svazijsko
+        760	SYR	SY	(ISO 3166-2)	Sýria
+        090	SLB	SB	(ISO 3166-2)	Šalamúnove ostrovy
+        724	ESP	ES	(ISO 3166-2)	Španielsko
+        744	SJM	SJ	(ISO 3166-2)	Špicbergy a Jan Mayen
+        756	CHE	CH	(ISO 3166-2)	Švajčiarsko
+        752	SWE	SE	(ISO 3166-2)	Švédsko
+        762	TJK	TJ	(ISO 3166-2)	Tadžikistan
+        158	TWN	TW	(ISO 3166-2)	Taiwan
+        380	ITA	IT	(ISO 3166-2)	Taliansko
+        834	TZA	TZ	(ISO 3166-2)	Tanzánia
+        764	THA	TH	(ISO 3166-2)	Thajsko
+        768	TGO	TG	(ISO 3166-2)	Togo
+        772	TKL	TK	(ISO 3166-2)	Tokelau
+        776	TON	TO	(ISO 3166-2)	Tonga
+        780	TTO	TT	(ISO 3166-2)	Trinidad a Tobago
+        788	TUN	TN	(ISO 3166-2)	Tunisko
+        792	TUR	TR	(ISO 3166-2)	Turecko
+        795	TKM	TM	(ISO 3166-2)	Turkménsko
+        796	TCA	TC	(ISO 3166-2)	Turks a Caicos
+        798	TUV	TV	(ISO 3166-2)	Tuvalu
+        800	UGA	UG	(ISO 3166-2)	Uganda
+        804	UKR	UA	(ISO 3166-2)	Ukrajina
+        858	URY	UY	(ISO 3166-2)	Uruguaj
+        860	UZB	UZ	(ISO 3166-2)	Uzbekistan
+        548	VUT	VU	(ISO 3166-2)	Vanuatu
+        336	VAT	VA	(ISO 3166-2)	Vatikán
+        862	VEN	VE	(ISO 3166-2)	Venezuela
+        162	CXR	CX	(ISO 3166-2)	Vianočný ostrov
+        704	VNM	VN	(ISO 3166-2)	Vietnam
+        626	TLS	TL	(ISO 3166-2)	Východný Timor
+        876	WLF	WF	(ISO 3166-2)	Wallis a Futuna
+        894	ZMB	ZM	(ISO 3166-2)	Zambia
+        732	ESH	EH	(ISO 3166-2)	Západná Sahara
+        716	ZWE	ZW	(ISO 3166-2)	Zimbabwe
+    }
+    }
+}
