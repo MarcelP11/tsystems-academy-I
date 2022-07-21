eef1268f8c51de3262b78c5f94f8002f588ff246
Marcel Polivcak <marcel.polivcak@centrum.sk>
2022-07-21 15:10:45 +0200

    21072022 hodina

--- IO/io/ListDir.java
+++ IO/io/ListDir.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class ListDir {
     public static void main(String[] args) {
         File dir = new File(".");
-
+/*
         if (dir.isDirectory()) {
             System.out.println("Listing of: " + dir.getAbsolutePath());
 
@@ -15,6 +15,31 @@ public class ListDir {
             }
         } else {
             System.err.printf("File %s is not directory", dir);
+        }
+        */
+
+        listDir2("C:/Users/Student/Desktop/academy");
+    }
+
+
+    private static void listDir(String path) {
+        System.out.println(path);
+        File directory = new File(path);
+        for (String fileName : directory.list()) {
+            System.out.println(fileName);
+            if (directory.isDirectory()) {
+                listDir(path + "/" + fileName);
+            }
+        }
+    }
+//dokoncit
+    private static void listDir2(String path){
+        File f=new File(path);
+        System.out.println("   "+f.getAbsolutePath());
+        if (f.isDirectory()){
+            for(String sub :f.listFiles()){
+                listDir2(sub);
+            }
         }
     }
 }
