369ce2f0aa7f01b46a890a5ee4429bff9d9c05a4
Marcel <marcel.polivcak@centrum.sk>
2022-07-22 08:12:46 +0200

    domaca 21072022

--- IO/io/ListDir.java
+++ IO/io/ListDir.java
@@ -1,10 +1,19 @@
 package io;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListDir {
     public static void main(String[] args) {
-        File dir = new File(".");
+        String path = ".";
+        //listDir(new File(path)); //alebo to mozeme takto napisat
+        //listDir(path);  //ak je cesta tak sa vykona metoda listDir(File f) teda o riadok vyssie
+        File f=new File(path);
+        List<File> allFiles = new ArrayList<>();
+        File
+        list(allFiles);
+        //File dir = new File(".");
 /*
         if (dir.isDirectory()) {
             System.out.println("Listing of: " + dir.getAbsolutePath());
@@ -16,7 +25,7 @@ public class ListDir {
         } else {
             System.err.printf("File %s is not directory", dir);
         }
-        */
+
 
         listDir2("C:/Users/Student/Desktop/academy");
     }
@@ -41,5 +50,39 @@ public class ListDir {
                 listDir2(sub);
             }
         }
+
+          */
+
+
     }
+
+    private static void listDir(String fileName) {
+        listDir(new File(fileName));  //zavola sa metoda nizsie ktora vytvori objekt triedy File
+    }
+
+    private static void listDir(File f) {
+        System.out.println("   " + f.getName());  //vypise  nazov
+        if (f.isDirectory()) {   //ak je subor adresarom tak sa vykona for slucka
+            for (File sub : f.listFiles()) {   //slucka sa vykona v danom prieinku a ak je nejaky prvok directory tak sa znova
+                listDir(sub);    //zavola listDir ale uz na tento adresar (rekurzia)
+            }
+        }
+    }
+
+    public static void list(List<File> files){
+        files.stream().forEach(file -> System.out.println(file.getName()));
+    }
+/*
+    private static List<File> filter(String path, String name) {
+        List<File> filtered = new ArrayList<File>();
+        File dir = new File(name);
+        filter(dir, filtered);
+        return filtered;
+    }
+
+    private static void filter(File f, list<File>, filtered){
+    if
+*/
+
 }
+
